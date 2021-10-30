42f60d5b4f6362132f591d7947eaa80ca977ef6e
renuka13599 <renuka13599@gmail.com>
2021-10-30 21:56:36 +0530

    Added:Print the maximum 1 st postion is same number

--- src/com/bridgelabz/Generics.java
+++ src/com/bridgelabz/Generics.java
@@ -1,25 +1,22 @@
 package com.bridgelabz;
 
 public class Generics {
-    public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
-        T max = x; // assume x is largest
-
-        if (y.compareTo(max) > 0)
-            max = y; // assume y is largest
-
-        if (z.compareTo(max) > 0)
-            max = z; // assume z is largest
-
-        return max; // returns the largest object
+    public static <T extends Comparable<T>> T maximum(T... elements) {
+        T max = elements[0];
+        for (T element : elements) {
+            if (element.compareTo(max) > 0) {
+                max = element;
+            }
+            else if(element.compareTo(max)>0)
+            {
+                max=element;
+            }
+        }
+        return max;
     }
-
     public static void main(String args[]) {
-        System.out.printf("Maximum number of (%d,%d,%d) is : %d\n\n", 102, 120,250, maximum(102, 120, 250)); //print the largest Integer number
-        System.out.printf("Maximum of (%.1f, %.1f , %.1f) is : %.1f\n\n", 10.6, 2.8, 7.7, maximum(10.6, 2.8, 7.7)); //print the largest Float number
-        System.out.printf("Maximum of (%s, %s,%s) is : %s\n", "mango", "apple", "icecream", maximum("mango", "apple", "icecraem"));//print the largest String
-
+        System.out.printf("Maximum number of (%d,%d,%d) is : %d\n\n", 12, 81, 10, maximum(12, 8, 10));
     }
-}
 
 
 
