f271c755d83eec8ca87abdbd76d646f89f9f5e41
renuka13599 <renuka13599@gmail.com>
2021-10-30 21:10:35 +0530

    Added:Print the maximum of integer,float and string

--- src/com/bridgelabz/Generics.java
+++ src/com/bridgelabz/Generics.java
@@ -1,21 +1,21 @@
 package com.bridgelabz;
 
 public class Generics {
-    public static void main(String[] args) {
-        Integer s1 = 10;
-        Integer s2 = 20;
-        Integer s3 = 30;
-        System.out.println(getMax(s1,s2,s3));
-    }
+        public static <T extends Comparable<T>> T maximum(T... elements) {
+            T max = elements[0];
+            for (T element : elements) {
+                if (element.compareTo(max) > 0) {
+                    max = element;
+                }
+            }
+            return max;// returns the largest object
+        }
+        public static void main(String args[]) {
+            System.out.printf("Maximum number of (%d,%d,%d,%d) is : %d\n\n", 12, 8, 5, 13, maximum(12, 8, 5, 13));
+            System.out.printf("Max of %.1f,%.1f,%.1f,%.1f is : %.1f\n\n", 6.6, 5.5, 10.1, 8.8, 7.7, maximum( 6.6, 5.5, 10.1, 8.8, 7.7 ));
+            System.out.printf("Max of %s, %s, %s, %s is : %s\n","pear", "apple", "orange", "pineapple", maximum("pear", "apple", "orange", "pineapple"));
 
-    private static <T extends Comparable> T getMax(T a, T b, T c) {
-
-        if (a.compareTo(b) > 0 && a.compareTo(c) > 0)
-            return a;
-        else if (b.compareTo(a) > 0 && b.compareTo(c) > 0)
-            return b;
-        else
-            return c;
+        }
     }
 }
 
