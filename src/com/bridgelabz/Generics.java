f56367f85ab17d3555b194ee218bb2b49080cbab
Renuka13599 <91589849+Renuka13599@users.noreply.github.com>
2021-10-30 21:15:39 +0530

    Update Generics.java

--- src/com/bridgelabz/Generics.java
+++ src/com/bridgelabz/Generics.java
@@ -1,4 +1,8 @@
 package com.bridgelabz;
+/**Author:Renu
+* Date:30/10/21
+* Purpose:Extend the max method to take more then three parameters:
+*/
 
 public class Generics {
         public static <T extends Comparable<T>> T maximum(T... elements) {
@@ -11,9 +15,9 @@ public class Generics {
             return max;// returns the largest object
         }
         public static void main(String args[]) {
-            System.out.printf("Maximum number of (%d,%d,%d,%d) is : %d\n\n", 12, 8, 5, 13, maximum(12, 8, 5, 13));
-            System.out.printf("Max of %.1f,%.1f,%.1f,%.1f is : %.1f\n\n", 6.6, 5.5, 10.1, 8.8, 7.7, maximum( 6.6, 5.5, 10.1, 8.8, 7.7 ));
-            System.out.printf("Max of %s, %s, %s, %s is : %s\n","pear", "apple", "orange", "pineapple", maximum("pear", "apple", "orange", "pineapple"));
+            System.out.printf("Maximum number of (%d,%d,%d,%d) is : %d\n\n", 12, 18, 5, 10, maximum(12, 18, 5, 10));
+            System.out.printf("Max of %.1f,%.1f,%.1f,%.1f is : %.1f\n\n", 6.6, 5.0, 13.1, 18.8, 6.7, maximum( 6.6, 5.0, 13.1, 18.8, 7.7 ));
+            System.out.printf("Max of %s, %s, %s, %s is : %s\n","lemon", "apple", "grapes", "pineapple", maximum("lemon", "apple", "grapes", "pineapple"));
 
         }
     }
