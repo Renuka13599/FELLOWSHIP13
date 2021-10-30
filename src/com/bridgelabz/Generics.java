b4e5df5b3421c40bd374a20ddb680b84c8b524c7
renuka13599 <renuka13599@gmail.com>
2021-10-30 21:39:51 +0530

    Added:Print the maximum of integer,float and string using refactor method

--- src/com/bridgelabz/Generics.java
+++ src/com/bridgelabz/Generics.java
@@ -1,21 +1,26 @@
 package com.bridgelabz;
 
 public class Generics {
-        public static <T extends Comparable<T>> T maximum(T... elements) {
-            T max = elements[0];
-            for (T element : elements) {
-                if (element.compareTo(max) > 0) {
-                    max = element;
-                }
-            }
-            return max;// returns the largest object
-        }
-        public static void main(String args[]) {
-            System.out.printf("Maximum number of (%d,%d,%d,%d) is : %d\n\n", 12, 8, 5, 13, maximum(12, 8, 5, 13));
-            System.out.printf("Max of %.1f,%.1f,%.1f,%.1f is : %.1f\n\n", 6.6, 5.5, 10.1, 8.8, 7.7, maximum( 6.6, 5.5, 10.1, 8.8, 7.7 ));
-            System.out.printf("Max of %s, %s, %s, %s is : %s\n","pear", "apple", "orange", "pineapple", maximum("pear", "apple", "orange", "pineapple"));
+    public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+        T max = x; // assume x is largest
 
-        }
+        if (y.compareTo(max) > 0)
+            max = y; // assume y is largest
+
+        if (z.compareTo(max) > 0)
+            max = z; // assume z is largest
+
+        return max; // returns the largest object
+    }
+
+    public static void main(String args[]) {
+        System.out.printf("Maximum number of (%d,%d,%d) is : %d\n\n", 102, 120,250, maximum(102, 120, 250)); //print the largest Integer number
+        System.out.printf("Maximum of (%.1f, %.1f , %.1f) is : %.1f\n\n", 10.6, 2.8, 7.7, maximum(10.6, 2.8, 7.7)); //print the largest Float number
+        System.out.printf("Maximum of (%s, %s,%s) is : %s\n", "mango", "apple", "icecream", maximum("mango", "apple", "icecraem"));//print the largest String
+
     }
 }
+
+
+
 
